5e91700ee55d87d9a495d6a4209aa4792de541f6
Seung-gi Min <110602191+seunGit@users.noreply.github.com>
2022-11-18 21:09:53 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        
+        System.out.println((double)a/b);
+    }
+}
