c875093be1e17dd2de05b6ecb31ae11f87c1ddb2
Seung-gi Min <110602191+seunGit@users.noreply.github.com>
2022-11-22 15:41:33 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean solution(String s) {
+        if ((s.length() == 4 || s.length() == 6) && s.matches("[0-9]+")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
