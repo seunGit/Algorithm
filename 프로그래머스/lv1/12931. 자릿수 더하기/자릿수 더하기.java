eb3f6d9c55ce8cd2737e8934af6834efc85b51cd
Seung-gi Min <110602191+seunGit@users.noreply.github.com>
2022-11-23 15:53:23 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.16 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        for (int i = 0; i < str.length(); i++) {
+            answer += Integer.parseInt(str.substring(i,i+1));
+        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
