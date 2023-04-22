02d32c1206a31ba0d64f59b70cb908fbd512de01
Seung-gi Min <110602191+seunGit@users.noreply.github.com>
2023-04-23 03:17:10 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,9 +1,11 @@
 class Solution {
     public String solution(String my_string, int k) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < k; i++) {
-            sb.append(my_string);
-        }
-        return sb.toString();
+    //     StringBuffer sb = new StringBuffer();
+    //     for (int i = 0; i < k; i++) {
+    //         sb.append(my_string);
+    //     }
+    //     return sb.toString();
+    // }
+        return my_string.repeat(k);
     }
 }
