bad19d1a283b2b110826e3bee09ef9fe534fd2c4
Seung-gi Min <110602191+seunGit@users.noreply.github.com>
2023-06-05 08:05:20 +0900

    [unrated] Title: l로 만들기, Time: 5.19 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        StringBuffer sb = new StringBuffer();
+        for (char ch : myString.toCharArray()) {
+            if (ch < 'l') sb.append('l');
+            else sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
